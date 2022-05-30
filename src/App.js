0b946ae6e62f47508f65bd7443c7ab52b7f66a3c
Tyler <68185249+tywil04@users.noreply.github.com>
2022-05-30 21:50:23 +0100

    Clean up App.js and remove tabular interface

--- src/App.js
+++ src/App.js
@@ -6,31 +6,10 @@ import { ContextMenu2, Tooltip2 } from "@blueprintjs/popover2";
 export default function App() {
   const [dialogOpen, setDialogOpen] = React.useState(false);
 
-  const [numberOfGeneratedTabs, setNumberOfGeneratedTabs] = React.useState(0);
-  const [selectedTab, setSelectedTab] = React.useState(`untitled-0`);
-  const [tabs, setTabs] = React.useState([]);
-  const [lastTabContextMenu, setLastTabContextMenu] = React.useState();
-
   window.onContextMenu = (event) => {
     event.preventDefault(); 
     event.stopPropagation();
   };
-
-  const newTab = () => {
-    setNumberOfGeneratedTabs(numberOfGeneratedTabs + 1);
-    console.log(numberOfGeneratedTabs)
-    setTabs(tabs.concat([{title: "Untitled", id: `untitled-${numberOfGeneratedTabs}`}]))
-  }
-
-  const removeTab = (id) => {
-    var tabsCopy = tabs;
-    tabs.forEach((tab) => {
-      console.log(tab.id)
-      if (tab.id === id) {
-        tabsCopy.pop(tab)
-      }
-    })
-  }
 
   return (
     <>
@@ -46,31 +25,35 @@ export default function App() {
         <div data-tauri-drag-region className="titlebar">
           <div className="titlebar:left">
             <ButtonGroup minimal small>
-              <Button small text="Open" onClick={() => setDialogOpen(true)}/>
-              <Button small text="New" onClick={newTab}/>
-              <Button small text="Save" onClick={() => setDialogOpen(true)}/>
+              <Button small text="Open"/>
+              <Button small text="New"/>
+              <Button small text="Save"/>
             </ButtonGroup>
           </div>
 
           <div className="titlebar:right">
             <ButtonGroup minimal small>
-            <Button small text="Preview" onClick={() => document.querySelector("body").classList.toggle("bp4-dark")}/>
-              <Button small text="Edit" active onClick={() => document.querySelector("body").classList.toggle("bp4-dark")}/>
+              <Button small text="Preview"/>
+              <Button small text="Edit" active/>
             </ButtonGroup>
 
             <Divider/>
 
             <ButtonGroup minimal small>
               <Tooltip2 hoverOpenDelay={350} content="Text Search">
-                <Button small icon="search-text" onClick={() => setDialogOpen(true)}/>
+                <Button small icon="search-text"/>
               </Tooltip2>
 
               <Tooltip2 hoverOpenDelay={350} content="Encrypt Contents">
-                <Button small icon="lock" onClick={() => setDialogOpen(true)}/>
+                <Button small icon="lock"/>
+              </Tooltip2>
+
+              <Tooltip2 hoverOpenDelay={350} content="Toggle Theme">
+                <Button small icon="moon" onClick={() => document.querySelector("body").classList.toggle("bp4-dark")}/>
               </Tooltip2>
 
               <Tooltip2 hoverOpenDelay={350} content="Settings">
-                <Button small icon="cog" onClick={() => setDialogOpen(true)}/>
+                <Button small icon="cog"/>
               </Tooltip2>
             </ButtonGroup>
 
@@ -83,22 +66,6 @@ export default function App() {
             </ButtonGroup>
           </div>
         </div>
-      </ContextMenu2>
-
-      <ContextMenu2
-        content={
-          <Menu>
-            <MenuItem text="Close 'Untitled'" intent="danger" onClick={() => removeTab(lastTabContextMenu)}/>
-          </Menu>
-        }>
-
-        <Tabs animate={false} selectedTabId={selectedTab} className="tab:bar" onChange={setSelectedTab}>
-          {tabs.map((tab) => 
-            <Tab className="tab" id={tab.id} data-tab-id={tab.id} onContextMenu={(event) => setLastTabContextMenu(event.target.getAttribute("data-tab-id"))}>
-              {tab.title}
-            </Tab>
-          )}
-        </Tabs>
       </ContextMenu2>
 
       <ContextMenu2
@@ -124,34 +91,17 @@ export default function App() {
         <div className={Classes.DIALOG_BODY}>
           <p>
             <strong>
-              Data integration is the seminal problem of the digital age. For over ten years, we’ve helped the
-              world’s premier organizations rise to the challenge.
+              Dialog Testing
             </strong>
           </p>
           <p>
-            Palantir Foundry radically reimagines the way enterprises interact with data by amplifying and extending
-            the power of data integration. With Foundry, anyone can source, fuse, and transform data into any shape
-            they desire. Business analysts become data engineers — and leaders in their organization’s data
-            revolution.
+            This is a working demo of a dialog. This will be used to prompt the user for information. Such as a password to Encrypt/Decrypt content
           </p>
-          <p>
-            Foundry’s back end includes a suite of best-in-class data integration capabilities: data provenance,
-            git-style versioning semantics, granular access controls, branching, transformation authoring, and more.
-            But these powers are not limited to the back-end IT shop.
-          </p>
-          <p>
-            In Foundry, tables, applications, reports, presentations, and spreadsheets operate as data integrations
-            in their own right. Access controls, transformation logic, and data quality flow from original data
-            source to intermediate analysis to presentation in real time. Every end product created in Foundry
-            becomes a new data source that other users can build upon. And the enterprise data foundation goes where
-            the business drives it.
-          </p>
-          <p>Start the revolution. Unleash the power of data integration with Palantir Foundry.</p>
         </div>
         <div className={Classes.DIALOG_FOOTER}>
           <div className={Classes.DIALOG_FOOTER_ACTIONS}>
             <Button small text="Close" onClick={() => setDialogOpen(false)}/>
-            <Button small intent={"primary"} text="Confirm"/>
+            <Button small intent={"primary"} text="Confirm" onClick={() => setDialogOpen(false)}/>
           </div>
         </div>
       </Dialog>
