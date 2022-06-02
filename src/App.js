c3ec61c2d83fdf484cf6747043d4c930afae1c24
Tyler <68185249+tywil04@users.noreply.github.com>
2022-06-02 13:50:30 +0100

    Added 'open' file feature + added discard dialog when closing unsaved files

--- src/App.js
+++ src/App.js
@@ -12,14 +12,18 @@ import { ContextMenu2, Tooltip2 } from "@blueprintjs/popover2";
 // Settings:
 //  Theme selector: Light/Dark/Custom?
 //  Text wrapping: Bool
-// 
-// Open Functionality
+//  Tabs (e.g spaces vs tab), if spaces how many etc
+//  Autosave
+//    
+// Userfeed back (e.g: Failed to save, try again etc)
+//
 // Encrypting Contents Functionality 
 // 
 // Etc
 
 export default function App() {
   const [currentFilePath, setCurrentFilePath] = React.useState("");
+  const [textEdited, setTextEdited] = React.useState(false);
   const fileNameRef = React.useRef();
   const statisticsDisplayRef = React.useRef();
   const textEditorRef = React.useRef();
@@ -30,53 +34,58 @@ export default function App() {
     event.stopPropagation();
   };
 
-  // Handles the logic behind the stats display
-  const calculateWordsAndCharacters = (event) => {
-    var words = event.target.value.trim().replace("\n", " ").split(/(\s+)/).filter((word) => word.trim().length > 0).length;
-    var characters = event.target.value.replace("\n", "").replace(" ", "").length
+  const calculateWordsAndCharacters = () => {
+    var words = textEditorRef.current.value.trim().replace("\n", " ").split(/(\s+)/).filter((word) => word.trim().length > 0).length;
+    var characters = textEditorRef.current.value.replace("\n", "").replace(" ", "").length
     statisticsDisplayRef.current.innerText = `${words} Words, ${characters} Characters`
   }
 
   // Get file name from path using a regular expression
+  // eslint-disable-next-line
   const getFileNameFromPath = (filePath) => filePath.replace(/^.*(\\|\/|\:)/, "");
 
+  // Generate a random hexadecimal string
   const generateRandomString = () => Math.random().toString(16).substr(2, 14);
 
   // Renders a dialog that returns a promise. A success is the save button being pressed, a failure is the delete button being pressed
   const runDiscardDialog = () => {
     return new Promise((success, failure) => {
-      const random = "discard-dialog-" + generateRandomString();
+      try {
+        const random = "discard-dialog-" + generateRandomString();
 
-      // Create a div with a random id that will be used to hold the dialog
-      var div = document.createElement("div")
-      div.id = random;
-      document.querySelector("body").appendChild(div);
+        // Create a div with a random id that will be used to hold the dialog
+        var div = document.createElement("div")
+        div.id = random;
+        document.querySelector("body").appendChild(div);
 
-      // Dialog component
-      const InternalDialog = (props) => {
-        const [open, setOpen] = React.useState(true);
-        return (
-          <Dialog usePortal={false} title="Discard Contents" isOpen={open} onClose={() => setOpen(false)} onClosed={() => document.getElementById(random).remove()}>
-            <div className={Classes.DIALOG_BODY}>
-              <p>
-                What would you like to do with this file? You can discard it, save it or continue editing it.
-              </p>
-            </div>
-            <div className={Classes.DIALOG_FOOTER}>
-              <div className={Classes.DIALOG_FOOTER_ACTIONS}>
-                <Button small text="Continue Editing" onClick={() => setOpen(false)}/>
-                <Button small intent="danger" text="Discard" onClick={() => {setOpen(false); props.failure()}}/>
-                <Button small intent="primary" text="Save" onClick={() => {setOpen(false); props.success()}}/>
+        // Dialog component
+        const InternalDialog = (props) => {
+          const [open, setOpen] = React.useState(true);
+          return (
+            <Dialog usePortal={false} title="Discard Contents" isOpen={open} onClose={() => setOpen(false)} onClosed={() => document.getElementById(random).remove()}>
+              <div className={Classes.DIALOG_BODY}>
+                <p>
+                  What would you like to do with this file? You can discard it, save it or continue editing it.
+                </p>
               </div>
-            </div>
-          </Dialog>
-        )
-      }
+              <div className={Classes.DIALOG_FOOTER}>
+                <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+                  <Button small text="Continue Editing" onClick={() => {setOpen(false); props.success("continue")}}/>
+                  <Button small intent="danger" text="Discard" onClick={() => {setOpen(false); props.success("discard")}}/>
+                  <Button small intent="primary" text="Save" onClick={() => {setOpen(false); props.success("save")}}/>
+                </div>
+              </div>
+            </Dialog>
+          )
+        }
 
-      // Render the dialog in the div that was created
-      ReactDOM.createRoot(document.getElementById(random)).render(
-        <InternalDialog success={success} failure={failure}/>
-      );
+        // Render the dialog in the div that was created
+        ReactDOM.createRoot(document.getElementById(random)).render(
+          <InternalDialog success={success} failure={failure}/>
+        );
+      } catch {
+        failure();
+      }
     })
   }
 
@@ -85,52 +94,113 @@ export default function App() {
       writeFile({contents: textEditorRef.current.value, path: filePath});
       fileNameRef.current.innerText = getFileNameFromPath(filePath);
       setCurrentFilePath(filePath);
+      setTextEdited(false)
     })
   }
 
   const saveFile = () => {
+    // If the current filePath is known then save the file
     if (currentFilePath !== "") {
-      try {
-        writeFile({contents: textEditorRef.current.value, path: currentFilePath});
-      } catch {
-        return saveFileAs();
-      }
+      return new Promise((success, failure) => {
+        try {
+          writeFile({contents: textEditorRef.current.value, path: currentFilePath});
+          setTextEdited(false);
+          success();
+        } catch {
+          failure();
+        }
+      })
+    // If the current filePath is not known then ask where to save the file
     } else {
       return saveFileAs();
     }
   }
 
-  const clearFile = () => {
+  const clear = () => {
     textEditorRef.current.value = "";
     setCurrentFilePath("");
-    fileNameRef.current.innerText = "untitled";
+    fileNameRef.current.innerText = "";
+    setTextEdited(false);
+    calculateWordsAndCharacters();
   }
 
   const newFile = () => {
-    if (textEditorRef.current.value !== "") {
-      const discardDialog = runDiscardDialog();
-      discardDialog.then(
-        () => {
-          const file = saveFile();
-          file.then(clearFile)
-        } ,
-        clearFile,
+    // If text has not been edited, run clear the textarea and stats display
+    if (textEdited === false) {
+      clear();
+    // If the text has been edited and the text isnt an empty string, then run `runDiscardDialog` and await the response
+    } else if (textEditorRef.current.value !== "") {
+      runDiscardDialog().then(
+        (response) => {
+          switch(response) {
+            case "save": {
+              const file = saveFile();
+              file.then(clear);
+              break;
+            };
+            case "discard": clear(); break;
+            default: break;
+          }
+        },
       )
     }
   }
 
-  const openFile = () => {
-    return;
+  const open = () => {
+    clear(); // Clear all
+    openDialog().then((filePath) => {
+      readTextFile(filePath).then((text) => {
+        textEditorRef.current.value = text;
+        setCurrentFilePath(filePath);
+        fileNameRef.current.innerText = getFileNameFromPath(filePath);
+        calculateWordsAndCharacters(); // Update words and characters (It should be 0, however its best to run the function)
+      })
+    })
   }
 
-  // const openFile = () => {
-  //   openDialog().then((filePath) => {
-  //     readTextFile(filePath).then((text) => 
-  //       textEditorRef.current.value = text
-  //     )
-  //     fileNameRef.current.innerText = filePath;
-  //   })
-  // }
+  const openFile = () => {
+    // If text has not been edited, run the `open` system dialog
+    if (textEdited === false) {
+      open();
+    // If the text has been edited and the text isnt an empty string, then run `runDiscardDialog` and await the response
+    } else if (textEditorRef.current.value !== "") {
+      runDiscardDialog().then(
+        (response) => {
+          switch(response) {
+            case "save": {
+              const file = saveFile();
+              file.then(open);
+              break;
+            };
+            case "discard": open(); break;
+            default: break;
+          }
+        },
+      )
+    }
+  }
+
+  const closeApplication = () => {
+    // If text has not been edited, close application
+    if (textEdited === false) {
+      appWindow.close();
+    // If the text has been edited and the text isnt an empty string, then run `runDiscardDialog` and await the response
+    } else if (textEditorRef.current.value !== "") {
+      runDiscardDialog().then(
+        (response) => {
+          switch(response) {
+            case "save": {
+              const file = saveFile();
+              file.then(appWindow.close);
+              break;
+            };
+            case "discard": appWindow.close(); break; 
+            default: break;
+          }
+        }
+      )
+    }
+  }
 
   return (
     <>
@@ -139,14 +209,14 @@ export default function App() {
           <Menu>
             <MenuItem icon="minus" text="Minimize" onClick={() => appWindow.minimize()}/>
             <MenuItem icon="plus" text="Maximize" onClick={() => appWindow.toggleMaximize()}/>
-            <MenuItem icon="cross" text="Close" intent="danger" onClick={() => appWindow.close()}/>
+            <MenuItem icon="cross" text="Close" intent="danger" onClick={closeApplication}/>
           </Menu>
         }>
 
         <div data-tauri-drag-region className="titlebar">
           <div className="titlebar:left">
             <ButtonGroup minimal small>
-              <Button small text="Open" className="titlebar:button"/>
+              <Button small text="Open" className="titlebar:button" onClick={openFile}/>
             </ButtonGroup>
 
             <Divider/>
@@ -183,13 +253,14 @@ export default function App() {
             <ButtonGroup small minimal>
               <Button small icon="minus" onClick={() => appWindow.minimize()}/>
               <Button small icon="small-square" onClick={() => appWindow.toggleMaximize()}/>
-              <Button small icon="cross" onClick={() => appWindow.close()}/>
+              <Button small icon="cross" onClick={closeApplication}/>
             </ButtonGroup>
           </div>
         </div>
         
         <div className="titlebar:lower">
-          <span ref={fileNameRef} className="titlebar:text titlebar:lower:left">untitled</span>
+          <span className="titlebar:text titlebar:lower:left" hidden={!textEdited}>~</span>
+          <span ref={fileNameRef} className="titlebar:text titlebar:lower:left"></span>
           <span ref={statisticsDisplayRef} className="titlebar:text titlebar:lower:right">0 Words,  0 Characters</span>
         </div>
       </ContextMenu2>
@@ -209,7 +280,7 @@ export default function App() {
           </Menu>
         }>
 
-        <textarea ref={textEditorRef} wrap="soft" spellCheck={false} onChange={calculateWordsAndCharacters} className="texteditor texteditor:nowrap"></textarea>
+        <textarea ref={textEditorRef} wrap="soft" spellCheck={false} onChange={() => {calculateWordsAndCharacters(); setTextEdited(true)}} className="texteditor texteditor:nowrap"></textarea>
       </ContextMenu2>      
     </>
   );
