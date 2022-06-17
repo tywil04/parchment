b9b2514d87d2d1add983caf9df23c90a6f1b9890
Tyler <68185249+tywil04@users.noreply.github.com>
2022-06-17 19:38:47 +0100

    a

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: "class",
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
